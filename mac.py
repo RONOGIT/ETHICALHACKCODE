26936b3c61d9e358c5a90ac79ff26653b1d43ed4
RONOGIT <141000339+RONOGIT@users.noreply.github.com>
2024-02-03 16:09:25 +0530

    Create mac.py

---
+++ mac.py
@@ -0,0 +1,4 @@
+import subprocess
+subprocess.call("ifconfig wlan0 down",shell=True)
+subprocess.call("if config wlan0 hw ether 00:11:22:33:44:55",shell= True)
+subprocess.call("ifconfig wlan0 up",shell=True)
