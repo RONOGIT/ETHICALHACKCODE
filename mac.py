232ee129a8344450b7ba5f3917fda844378978ab
RONOGIT <141000339+RONOGIT@users.noreply.github.com>
2024-02-22 12:37:54 +0530

    mac.py

--- mac.py
+++ mac.py
@@ -2,3 +2,6 @@ import subprocess
 subprocess.call("ifconfig wlan0 down",shell=True)
 subprocess.call("if config wlan0 hw ether 00:11:22:33:44:55",shell= True)
 subprocess.call("ifconfig wlan0 up",shell=True)
+
+
+""" cd PycharmProjects/""" """in root directory"""
